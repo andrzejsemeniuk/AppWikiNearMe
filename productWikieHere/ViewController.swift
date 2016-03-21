c3f345120bab5d9755f3977e91edc24c5eea368a
Andrzej Semeniuk <andrzej@tinygamefactory.com>
2016-03-21 12:27:33 -0500

    Initial Commit

---
+++ productWikieHere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  productWikieHere
+//
+//  Created by andrzej semeniuk on 3/21/16.
+//  Copyright © 2016 Tiny Game Factory LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
